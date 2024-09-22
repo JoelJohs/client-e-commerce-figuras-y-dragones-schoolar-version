9ab86c83530569c47fdf04648fe94d911250ed77
Joel Josafat Hernández Saucedo <jojohersa21@gmail.com>
2024-09-21 21:03:46 -0600

    return stores to basic setup

--- src/store/useCartTheme.js
+++ src/store/useCartTheme.js
@@ -1,29 +1,11 @@
 import { create } from "zustand";
 
-const getInitialCart = () => {
-  const savedCart = localStorage.getItem("cart");
-  return savedCart ? JSON.parse(savedCart) : [];
-};
-
 const useCartStore = create((set) => ({
-  cart: getInitialCart(),
-  addToCart: (item) =>
-    set((state) => {
-      const updatedCart = [...state.cart, item];
-      localStorage.setItem("cart", JSON.stringify(updatedCart));
-      return { cart: updatedCart };
-    }),
-  removeFromCart: (itemId) =>
-    set((state) => {
-      const updatedCart = state.cart.filter((item) => item.id !== itemId);
-      localStorage.setItem("cart", JSON.stringify(updatedCart));
-      return { cart: updatedCart };
-    }),
-  clearCart: () =>
-    set(() => {
-      localStorage.removeItem("cart");
-      return { cart: [] };
-    }),
+  cart: [],
+  addToCart: (product) => set((state) => ({ cart: [...state.cart, product] })),
+  removeFromCart: (product) =>
+    set((state) => ({ cart: state.cart.filter((p) => p.id !== product.id) })),
+  clearCart: () => set({ cart: [] }),
 }));
 
 export default useCartStore;
--- src/store/useThemeStore.js
+++ src/store/useThemeStore.js
@@ -1,30 +1,8 @@
 import { create } from "zustand";
 
-const useThemeStore = create((set) => {
-  const getSystemTheme = () => {
-    if (
-      window.matchMedia &&
-      window.matchMedia("(prefers-color-scheme: dark)").matches
-    ) {
-      return "dark";
-    }
-    return "light";
-  };
-
-  const getInitialTheme = () => {
-    const savedTheme = localStorage.getItem("theme");
-    return savedTheme ? savedTheme : getSystemTheme();
-  };
-
-  return {
-    theme: getInitialTheme(),
-    toggleTheme: () =>
-      set((state) => {
-        const newTheme = state.theme === "light" ? "dark" : "light";
-        localStorage.setItem("theme", newTheme);
-        return { theme: newTheme };
-      }),
-  };
-});
+const useThemeStore = create((set) => ({
+  isDark: false,
+  toggleTheme: () => set((state) => ({ isDark: !state.isDark })),
+}));
 
 export default useThemeStore;
