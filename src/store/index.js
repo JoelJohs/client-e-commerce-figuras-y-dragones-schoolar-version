3f1e26b33b8b6a649ac0321b6a304390d48b03b1
Joel Josafat Hernández Saucedo <jojohersa21@gmail.com>
2024-09-21 20:55:42 -0600

    add stores with Zustand

--- src/store/index.js
+++
@@ -1,2 +0,0 @@
-export { default as useCartStore } from "./useCartStore";
-export { default as useThemeStore } from "./useThemeStore";
--- src/store/useThemeStore.js
+++ src/store/useThemeStore.js
@@ -1,30 +1,30 @@
 import { create } from "zustand";
 
 const useThemeStore = create((set) => {
-    const getSystemTheme = () => {
-        if (
-            window.matchMedia &&
-            window.matchMedia("(prefers-color-scheme: dark)").matches
-        ) {
-            return "dark";
-        }
-        return "light";
-    };
+  const getSystemTheme = () => {
+    if (
+      window.matchMedia &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches
+    ) {
+      return "dark";
+    }
+    return "light";
+  };
 
-    const getInitialTheme = () => {
-        const savedTheme = localStorage.getItem("theme");
-        return savedTheme ? savedTheme : getSystemTheme();
-    };
+  const getInitialTheme = () => {
+    const savedTheme = localStorage.getItem("theme");
+    return savedTheme ? savedTheme : getSystemTheme();
+  };
 
-    return {
-        theme: getInitialTheme(),
-        toggleTheme: () =>
-            set((state) => {
-                const newTheme = state.theme === "light" ? "dark" : "light";
-                localStorage.setItem("theme", newTheme);
-                return { theme: newTheme };
-            }),
-    };
+  return {
+    theme: getInitialTheme(),
+    toggleTheme: () =>
+      set((state) => {
+        const newTheme = state.theme === "light" ? "dark" : "light";
+        localStorage.setItem("theme", newTheme);
+        return { theme: newTheme };
+      }),
+  };
 });
 
 export default useThemeStore;
